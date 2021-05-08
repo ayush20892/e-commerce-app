a61249799d80e2b1f98bda48970094f45b2ace72
ayush20892 <ayush20892@gmail.com>
2021-05-09 00:43:39 +0530

    style: footer resize

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,35 +1,48 @@
 import "./footer.css";
-import { BsHeart,BsHeartFill, BsInboxes } from "react-icons/bs";
+import { BsHeart, BsHeartFill, BsInboxes } from "react-icons/bs";
 import { AiOutlineHome } from "react-icons/ai";
 import { HiOutlineUserCircle } from "react-icons/hi";
-import { Link } from "react-router-dom"
-import { useWishList } from "../../context/wishListContext.js"
+import { Link } from "react-router-dom";
+import { useWishList } from "../../context/wishListContext.js";
 
-
-export function Footer(){
-  const { stateWishList } = useWishList()
-  return(
+export function Footer() {
+  const { stateWishList } = useWishList();
+  return (
     <footer>
       <div className="footer-icons">
-        <Link to="/" style={{textDecoration: "none", color: "black"}}>
-          <AiOutlineHome style={{fontSize: "2rem", marginTop: "0.8rem", cursor: "pointer"}}/>
+        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+          <AiOutlineHome
+            style={{ fontSize: "1.8rem", marginTop: "0.7rem", cursor: "pointer" }}
+          />
           <h4>Home</h4>
         </Link>
-        <Link to="/categories" style={{textDecoration: "none", color: "black"}}>
-          <BsInboxes style={{fontSize: "2rem", marginTop: "1rem"}}/>
+        <Link
+          to="/categories"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <BsInboxes style={{ fontSize: "1.8rem", marginTop: "0.9rem" }} />
           <h4>Category</h4>
         </Link>
 
-        <Link to="/wishList" style={{textDecoration: "none", color: "black"}}>
-        {stateWishList.itemsInWishList.length === 0 ?<BsHeart style={{fontSize: "2rem", marginTop: "0.9rem"}}/> 
-        :
-        <BsHeartFill style={{fontSize: "2rem", marginTop: "0.9rem", fill: "var(--primary-color"}}/>
-        }
+        <Link to="/wishList" style={{ textDecoration: "none", color: "black" }}>
+          {stateWishList.itemsInWishList.length === 0 ? (
+            <BsHeart style={{ fontSize: "1.8rem", marginTop: "0.8rem" }} />
+          ) : (
+            <BsHeartFill
+              style={{
+                fontSize: "1.8rem",
+                marginTop: "0.8rem",
+                fill: "var(--primary-color",
+              }}
+            />
+          )}
           <h4>WishList</h4>
         </Link>
 
-        <Link to="/login" style={{textDecoration: "none", color: "black"}}>
-          <HiOutlineUserCircle style={{fontSize: "2rem", marginTop: "1rem"}}/>
+        <Link to="/login" style={{ textDecoration: "none", color: "black" }}>
+          <HiOutlineUserCircle
+            style={{ fontSize: "1.8rem", marginTop: "0.9rem" }}
+          />
           <h4>My Account</h4>
         </Link>
       </div>

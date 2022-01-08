9355ff52c78ff625e98d723bd15f14e171d4d850
Ayush Sharma <ayush20892@gmail.com>
2022-01-09 00:04:07 +0530

    Utility Modified

--- src/categoryData.js
+++ src/categoryData.js
@@ -1,49 +1,51 @@
+import menCategory from "./components/categoryImages/menCategory.jpg";
+import womenCategory from "./components/categoryImages/womenCategory.jpg";
+import gadgetCategory from "./components/categoryImages/gadgetCategory.jpg";
+
 export const CategoryData = [
-  { 
+  {
     categoryName: "men",
-    categoryImg: "https://unusual-ecom.netlify.app/static/media/menCategory.230b14a2.jpg",
+    categoryImg: menCategory,
     productType: [
       {
         id: "men1",
         img: "https://images.bewakoof.com/t540/nimbus-grey-half-sleeve-t-shirt-men-s-plain-t-shirts-223291-1585903641.jpg",
         name: "Half Sleeve T-shirt",
-        page: "halfSleeveTshirt"
+        page: "halfSleeveTshirt",
       },
 
       {
         id: "men2",
         img: "https://images.bewakoof.com/t540/men-s-white-stripe-slim-fit-shirts-men-s-stripe-shirts-291870-1617617304.jpg",
         name: "Shirt",
-        page: "shirt"
-      }
-  ]
-},
+        page: "shirt",
+      },
+    ],
+  },
 
-  { 
+  {
     categoryName: "women",
-    categoryImg: "https://unusual-ecom.netlify.app/static/media/womenCategory.dc88410b.jpg",
+    categoryImg: womenCategory,
     productType: [
       {
         id: "women1",
         img: "https://images.bewakoof.com/t540/watermelon--aop-t-shirt-women-s-plain-half-sleeves-aop-t-shirts-338845-1614351440.jpg",
         name: "Half Sleeve T-shirt",
-        page: "halfSleeveTshirt"
+        page: "halfSleeveTshirt",
       },
 
       {
         id: "women2",
         img: "https://images.bewakoof.com/t540/women-s-ethnic-printed-top-women-s-ethnic-printed-top-318385-1614943408.jpg",
         name: "Kurti",
-        page: "kurti"
-      }
-    ]
+        page: "kurti",
+      },
+    ],
   },
 
   {
     categoryName: "gadget",
-    categoryImg: "https://unusual-ecom.netlify.app/static/media/gadgetCategory.274582ae.jpg",
-    productType: []
-  }
-]
-
-
+    categoryImg: gadgetCategory,
+    productType: [],
+  },
+];
--- src/util.js
+++ src/util.js
@@ -1,20 +1,21 @@
-export function CheckItem(itemsInCart, currItem) {
-  return itemsInCart.find(item => item.id === currItem.id);
+export function CheckItem(itemsInCart, productId) {
+  if (itemsInCart.length === 0) return false;
+  const itemPresent = itemsInCart.find(
+    (item) => item.product._id === productId
+  );
+  return itemPresent ? true : false;
 }
-
 
 export function GetKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
+  return Object.keys(object).find((key) => object[key] === value);
 }
 
-export function CategoryMatch(str)
-{
-    var result = str.match(/\b(men|women|gadget)\b/)
-    return result;
+export function CategoryMatch(str) {
+  var result = str.match(/\b(men|women|gadget)\b/);
+  return result;
 }
 
-export function ProductTypeMatch(str)
-{
-    var result = str.match(/(halfSleeveTshirt|shirt|kurti)/)
-    return result;
+export function ProductTypeMatch(str) {
+  var result = str.match(/(halfSleeveTshirt|shirt|kurti)/);
+  return result;
 }

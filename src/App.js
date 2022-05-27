107661bcc44dd4ab46f4e5d7f29ef74bfee6e5ef
Ayush Sharma <ayush20892@gmail.com>
2022-05-27 17:53:54 +0530

    refactor: localstorage auth

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     if (data.success)
       authDispatch({ type: "LOAD_PRODUCTS", payload: data.productResult });
 
-    if (session?.userId) {
+    if (session?.userId && session?.userId !== null) {
       const userData = await userDashboard();
 
       if (!userData.success) {
@@ -40,6 +40,9 @@ function App() {
       }
 
       authDispatch({ type: "START_SESSION", payload: userData.user });
+    } else {
+      authDispatch({ type: "END_SESSION" });
+      navigate("/user/login", { replace: "true" });
     }
     setIsLoading(false);
   }
